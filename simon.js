566556cb7599a299d3964a257f0375a23bca88d7
Lara Martin <larikimc@gmail.com>
2016-05-16 14:24:00 +0200

    trying change button colors

--- simon.js
+++ simon.js
@@ -6,30 +6,59 @@ $(document).ready(function(){
 
   var started = false;
   var counter = "--";
+  var userSeq = [];
 
   function displayCounts(num){
     var html = "<p id=\"counter_display\">" + num + "</p>";
     $("#counter_display").html(html);
   };
 
+  // function timer(buttonColor, lightCode){
+  //   var id = "#".concat(buttonColor);
+  //   var color = "#".concat(lightCode)
+  //   $(id).css({"border-color":color});
+  // }
+
+
   $(".button").click(function() {
-    var val = $(this).val();
+    var hasStopped = false;
+    // if (started){
+    //   // if (val === "start_button"){
+    //   console.log("started");
+    //   started = false;
+    //   counter = "--";
+    //   hasStopped = true;
+    //   //}  else {
+    //   //   started = true;
+    //   //   counter = 0;
+    //   //   hasStopped = false;
+    //   //   console.log("STOP");
+    //   // }
+    // }
+    var val = $(this).attr("id");
     console.log("val: " + val);
-    if (val === "start_button"){
-      // when pressing start, if already started, stop
-      if (started){
-        started = false;
-        counter = "--";
-        console.log("counter: " + counter);
-      } else {
-        started = true;
-        counter = 0;
-        console.log("else counter: " + counter);
-      }
-    }
     if (val === "left_upper_arc"){
-        console.log("left upper");
-      }
+      console.log("left upper");
+      // $("#left_upper_arc").css({"border-color":"#A9F5A9"});
+      $("#left_upper_arc").effect("pulsate", {times:1}, 40 );
+      // interval = setInterval(timer("left_upper_arc", "#A9F5A9"), 100);
+    } else if (val === "left_bottom_arc"){
+      console.log("left bottom");
+      $("#left_bottom_arc").css({"border-color":"#F2F5A9"});
+    } else if (val === "right_upper_arc"){
+      console.log("right upper");
+      $("#right_upper_arc").css({"border-color":"#F78181"});
+    } else if (val === "right_bottom_arc"){
+      console.log("right bottom");
+      $("#right_bottom_arc").css({"border-color":"#A9A9F5"});
+    }
+
+
+    userSeq.push(val);
+    console.log("userSeq: " + userSeq);
+    console.log("started: " + started);
+    console.log("counter: " + counter);
+    console.log("--------");
     displayCounts(counter);
   });
 
