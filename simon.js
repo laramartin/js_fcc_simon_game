73796d8a280aff71bf10e1dd313567be75a037f5
Lara Martin <larikimc@gmail.com>
2016-06-05 13:50:53 +0200

    Implemented game stats

--- simon.js
+++ simon.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 
-  var started = false;
   var counter = 0;
   //var machineSeq = ["left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc","left_upper_arc"];
   var machineSeq = [];
   var userSeq = [];
   var buttonOrder = ["left_upper_arc", "right_upper_arc", "left_bottom_arc", "right_bottom_arc"];
-  var userTurn = false; // if false, it's machine's turn
+  
+  // user    -> waiting for input (user turn)
+  // machine -> playing sequence
+  // initial -> initial state
+  var state = 'initial';
+
   var numGuesses = 0;
   var strict = false;
   var errorSound = new Audio("https://raw.githubusercontent.com/laramartin/js_fcc_simon_game/master/src/button-10.wav");
@@ -24,27 +28,31 @@ $(document).ready(function(){
     "https://raw.githubusercontent.com/laramartin/js_fcc_simon_game/master/src/simonSound4.mp3",
   ];
 
-  function reset(){
-    started = false;
+  function startGame(){
     counter = 0;
     machineSeq = [];
     userSeq = [];
     clearTimeout();
+    state = 'machine';
+    setTimeout(function() { machinePick(); }, 1000);
   }
 
   function buttonLedColor(button){
     var str = "#".concat(button);
-    if (strict || started){
+    if (strict || state !== 'initial'){
       $(str).css("box-shadow", "6px 6px 6px green");
     } else {
       $(str).css("box-shadow", "6px 6px 6px red");
     }
   }
+
+  // number of success guesses displayed on game
   function displayCounts(num){
     var html = "<p id=\"counter_display\">" + num + "</p>";
     $("#counter_display").html(html);
   };
 
+  // make button pulsation effect when tapped
   function buttonEffect(button){
     var str = "#".concat(button);
     var index = buttonOrder.indexOf(button) + 1;
@@ -53,13 +61,15 @@ $(document).ready(function(){
     sound.play();
   }
 
+  // get random number between 1-4 for machine guesses
   function randomButton(){
     var num = Math.floor((Math.random() * 4) + 1);
     return num;
   }
 
+  // iterate through machine picks and make button effect
   function loop(index){
-    userTurn = false;
+    state = 'machine';
     // 1 second setTimeout
     setTimeout(function () {
       buttonEffect(machineSeq[index]);
@@ -75,12 +85,13 @@ $(document).ready(function(){
 
   function startUserTurn(){
     numGuesses = 0;
-    userTurn = true;
+    state = 'user';
     userSeq = [];
   }
 
   // machine picks a button;
   function machinePick(){
+    state = 'machine';
     var pick = buttonOrder[randomButton() - 1];
     // push of machine's pick and do button effect of every item in array
     machineSeq.push(pick);
@@ -88,6 +99,7 @@ $(document).ready(function(){
     console.log("machineSeq: " + machineSeq);
   }
 
+  // compare machine & user picks
   function checkGuess(button, index){
     if (machineSeq[index - 1] !== button){
       errorSound.play();
@@ -97,29 +109,40 @@ $(document).ready(function(){
   }
 
   function userPick(button){
+    if (state !== 'user') {
+      // Ignore user pick outside the waiting for input
+      return;
+    }
+    // max times user can tap a button
     var maxPicks = machineSeq.length;
     numGuesses++;
     userSeq.push(button);
     console.log("userseq: " + userSeq);
     buttonEffect(button);
     var correct = checkGuess(button, numGuesses);
+    // if wrong and playing strict, restart
     if (!correct && strict){
-      reset();
+      startGame();
     } else if (!correct && !strict) {
       // repeat machine picks again, no adding a new one!!!!!!!!
-      loop(machineSeq);
+      state = 'machine';
+      console.log("wrong");
+      loop(0);
     } else if (correct && numGuesses == 20) {
+      // End of game
       alert("YOU WIN!!!!");
-    }
-    if (numGuesses >= maxPicks){
-      userTurn = false;
+    } else if (correct && numGuesses >= maxPicks) {
+      // End of user turn
+      counter++;
+      state = 'machine';
+      setTimeout(function() { machinePick(); }, 1000);
     }
   }
 
   $(".button").click(function() {
     var val = $(this).attr("id");
     if (val === "start_button"){
-      started = !started;
+      startGame();
       buttonLedColor(val);
     } else if (val === "strict_button") {
       strict = !strict;
@@ -128,25 +151,11 @@ $(document).ready(function(){
       } else {
         buttonLedColor(val);
       }
+    } else if (state === 'user') {
+      userPick(val);
     }
-    if (started){
-      if (userTurn){
-        userPick(val);
-      }
-      if (!userTurn){
-        if (machineSeq.length !== 0){
-          counter += 1;
-        }
-        setTimeout(function() { machinePick(); }, 1000);
-      }
-      if (val === "start_button"){
-        started = true;
-      }
-      displayCounts(counter);
-    }
-
+    displayCounts(counter);
     console.log("--------");
-
   });
 
   displayCounts("\n");
